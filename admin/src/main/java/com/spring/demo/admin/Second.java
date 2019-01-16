3f3c332064551756f59965057b7182329b4cd7eb
caoyilong <caoyilong@xmiles.cn>
2019-01-16 16:46:35 +0800

    提交2

---
+++ admin/src/main/java/com/spring/demo/admin/Second.java
@@ -0,0 +1,15 @@
+package com.spring.demo.admin;
+
+/**
+ * @author caoyilong
+ * @description
+ * @package com.spring.demo.admin
+ * @company xmiles
+ * @date 2019/1/16
+ */
+public class Second {
+
+    public static void main(String[] args) {
+        System.out.println("提交2");
+    }
+}
