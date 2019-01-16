9de27a0bf081154062b2435f90c5d94f323c2ba0
caoyilong <caoyilong@xmiles.cn>
2019-01-16 16:44:34 +0800

    提交1

---
+++ eureka-client/src/main/java/com/spring/demo/eurekaclient/demo/Demo.java
@@ -0,0 +1,16 @@
+package com.spring.demo.eurekaclient.demo;
+
+/**
+ * @author caoyilong
+ * @description
+ * @package com.spring.demo.eurekaclient.demo
+ * @company xmiles
+ * @date 2019/1/3
+ */
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("提交1");
+
+    }
+}
